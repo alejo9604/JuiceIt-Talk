0eb5d3b61e3f52d90bd8593530466e610e23a8f5
Alejandro Alvarez <juaalvarezme@gmail.com>
2024-03-12 22:08:25 -0500

    Fixed Projectile collision 2D. Added enemy object

--- Assets/Scripts/Projectile/Projectile.cs
+++ Assets/Scripts/Projectile/Projectile.cs
@@ -4,14 +4,14 @@ public class Projectile : MonoBehaviour
 {
     [SerializeField] private float _speed = 50f;
     [SerializeField] private float _lifeTime = 3;
-    
+
     private float _timeToDisable = -1;
-
-    private void Start()
-    {
-        _timeToDisable = Time.time + _lifeTime;
-    }
-
+    private LayerMask _collisionMask;
+    private ContactFilter2D _contactFilter;
+    private readonly RaycastHit2D[] _cachedContactHit = new RaycastHit2D[1];
+    
+    private const float _skinWidth = 0.1f;
+    
     public void Init(Vector3 dir, float extraSpeed, float lifeTime = 3)
     {
         _speed += extraSpeed;
@@ -19,6 +19,14 @@ public class Projectile : MonoBehaviour
         _timeToDisable = Time.time + _lifeTime;
         
         transform.up = dir;
+        
+        _collisionMask = 1 << LayerMask.NameToLayer("Player");
+        _collisionMask |= 1 << LayerMask.NameToLayer("Enemy");
+
+        _contactFilter = new ContactFilter2D();
+        _contactFilter.layerMask = _collisionMask;
+        _contactFilter.useLayerMask = true;
+        _contactFilter.useTriggers = true;
     }
 
     void Update()
@@ -33,7 +41,23 @@ public class Projectile : MonoBehaviour
         }
         
         float moveDistance = _speed * Time.deltaTime;
+        CheckCollision(moveDistance);
         transform.Translate(Vector3.up * moveDistance);
+    }
+    
+    void CheckCollision(float moveDistance)
+    {
+        int contacts = Physics2D.Raycast(transform.position, transform.forward, _contactFilter, _cachedContactHit,
+            moveDistance + _skinWidth);
+        if(contacts > 0)
+            OnHitObject(_cachedContactHit[0].collider, _cachedContactHit[0].point, _cachedContactHit[0].normal);
+    }
+    
+    private void OnHitObject(Collider2D collision, Vector2 hitPoint, Vector2 normalHitPoint)
+    {
+        //TryToApplyDamageToTarget(collision.transform, hitPoint);
+        Debug.Log($"Hit {collision.gameObject.name}");
+        DestroyProjectile();
     }
     
     private void DestroyProjectile()

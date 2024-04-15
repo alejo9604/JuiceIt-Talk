247dd0f3160d445681bf43a88e2a59b41417b057
Alejandro Alvarez <juaalvarezme@gmail.com>
2024-04-14 22:12:34 -0500

    Shadows

---
+++ Assets/Scripts/Player/Shadow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace AllieJoe.JuiceIt
+{
+    public class Shadow : MonoBehaviour
+    {
+        [SerializeField] private Transform _shadow;
+        private Vector2 _baseOffset;
+
+        private void Start()
+        {
+            _baseOffset = _shadow.localPosition;
+        }
+        
+        private void Update()
+        {
+            _shadow.localPosition = Quaternion.Euler(0, 0, -transform.eulerAngles.z) * _baseOffset;
+        }
+    }
+}
